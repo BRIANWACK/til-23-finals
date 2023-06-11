e0ef36836dfa5fc0d92f543c4832c613ad6e2cda
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-06-12 02:03:16 +0800

    use voice extraction in speaker id service

--- stubs/speaker_service.py
+++ stubs/speaker_service.py
@@ -2,12 +2,14 @@
 
 from pathlib import Path
 
-import librosa
 import numpy as np
 import torch
 from abstract_ai_services import AbstractSpeakerIDService
 from nemo.collections.asr.models import EncDecSpeakerLabelModel as NeMoModel
 from til_23_cv import cos_sim, thres_strategy_naive
+from torchaudio.functional import resample
+
+from til_23_finals.voice import VoiceExtractor
 
 BEST_DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -32,12 +34,14 @@ class NeMoSpeakerIDService(AbstractSpeakerIDService):
         # nvidia/speakerverification_en_titanet_large
         self.model: NeMoModel = NeMoModel.restore_from(restore_path=model_dir)
         self.model.to(device).eval()
+        self.extractor = VoiceExtractor()
 
         # TODO: Cache embeddings.
         self.speaker_ids = []
         self.speaker_embeds = []
         for wav_path in Path(speaker_dir).glob("*.wav"):
             speaker_id = wav_path.stem
+            # TODO: Should voice extraction also be applied here?
             embed = self.model.get_embedding(str(wav_path))
             self.speaker_ids.append(speaker_id)
             # Remove batch dimension.
@@ -45,6 +49,7 @@ class NeMoSpeakerIDService(AbstractSpeakerIDService):
 
         # Move to CPU to save GPU memory.
         self.model.to("cpu")
+        self.extractor.to("cpu")
 
     # TODO: Modify abstract interface to accept filepath instead of waveform for convenience.
     # TODO: Ability to exclude speaker ids if sure our team's speaker is already identified.
@@ -64,16 +69,19 @@ class NeMoSpeakerIDService(AbstractSpeakerIDService):
             string representing the speaker's ID corresponding to the list of speaker IDs in the training data set.
         """
         # `audio_waveform` is monochannel and has shape (n_samples,) already.
-        if sampling_rate != self.model_sr:
-            audio_waveform = librosa.resample(
-                audio_waveform, orig_sr=sampling_rate, target_sr=self.model_sr
-            )
-        audio_len = len(audio_waveform)
 
         with torch.inference_mode():
+            self.extractor.to(self.device)
+            wav = torch.tensor(audio_waveform, device=self.device)
+            wav, sr = self.extractor(wav, sampling_rate)
+            self.extractor.to("cpu")
+
+            wav = resample(wav, orig_freq=sr, new_freq=self.model_sr)
+            audio_len = len(wav)
+
             self.model.to(self.device)
             _, embed = self.model.forward(
-                input_signal=torch.tensor([audio_waveform], device=self.device),
+                input_signal=torch.tensor([wav], device=self.device),
                 input_signal_length=torch.tensor([audio_len], device=self.device),
             )
             self.model.to("cpu")
