4be6bc23c7418b50110a899c62056dfabb52f2ba
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-06-12 21:05:37 +0800

    refactor out AI loop

--- til_23_finals/__main__.py
+++ til_23_finals/__main__.py
@@ -10,29 +10,17 @@ are free to modify any thing in the "stubs" folder. You do not need to modify th
 import argparse
 import logging
 import time
-from pathlib import Path
 from typing import List
 
-import cv2
-import imutils
 import yaml
-
-# Import necessary and useful things from til2023 SDK
-from tilsdk import *  # import the SDK
-from tilsdk.reporting import save_zip  # to handle embedded zip file in flask response
+from tilsdk import LocalizationService, ReportingService
+from tilsdk.localization import GridLocation, RealLocation, RealPose, SignedDistanceGrid
 from tilsdk.utilities.filters import SimpleMovingAverage
 
-from .navigation import Navigator, plan_path
+from .ai import prepare_ai_loop
+from .navigation import Navigator
+from .planner import Planner
 
-# Import your code
-from .planner import (  # Exceptions for path planning.
-    InvalidStartException,
-    NoPathFoundException,
-    Planner,
-)
-from .utils import enable_camera, load_audio_from_dir
-
-# Setup logging in a nice readable format
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)5s][%(asctime)s][%(name)s]: %(message)s",
@@ -40,78 +28,16 @@ logging.basicConfig(
 )
 
 main_log = logging.getLogger("Main")
-reid_log = logging.getLogger("ReID")
-sid_log = logging.getLogger("SpeakID")
-cam_log = logging.getLogger("Cam")
 
 
-def reid(reid_service, hostage_img, suspect_img, img):
-    """Use reid service to identify whether hostage or suspect is in the image."""
-    reid_log.info(f"hostage img shape {hostage_img.shape}.")
-    reid_log.info(f"suspect img shape {suspect_img.shape}.")
-    reid_log.info(f"robot's photo shape {img.shape}.")
-
-    sus_bbox = reid_service.targets_from_image(img, suspect_img)
-    hostage_bbox = reid_service.targets_from_image(img, hostage_img)
-    main_log.info(f"suspect?: {sus_bbox};\n hostage?: {hostage_bbox}.")
-    return sus_bbox, hostage_bbox
-
-
-def identify_speakers(audio_dir: str, speakerid_service):
-    """Identify speakers from audio files in audio_dir."""
-    audio_dict = load_audio_from_dir(audio_dir)
-
-    speakerid_result = {}
-    for fname, v in audio_dict.items():
-        audio_waveform, rate = v
-        speakerid_result[fname] = speakerid_service.identify_speaker(
-            audio_waveform, rate
-        )
-
-    for fname, speaker_id in speakerid_result.items():
-        sid_log.info(f"{fname} speaker is {speaker_id}.")
-    return speakerid_result
-
-
-def detect_digits(digit_detection_service, audio_dir: str):
-    """Detect digits from audio files in audio_dir."""
-    digits_result = {}
-    audio_dict = load_audio_from_dir(audio_dir)
-    sorted_fnames = sorted(audio_dict.keys())
-
-    for fname in sorted_fnames:
-        audio_waveform, rate = audio_dict[fname]
-        digits = digit_detection_service.transcribe_audio_to_digits(audio_waveform)
-        digits_result[fname] = (
-            digits[0] if digits else None
-        )  # as a heuristic, take the first digit detected.
-    return digits_result
-
-
-# Implement core robot loop here.
 def main():
     """Run main loop."""
     # === Initialize admin services ===
+    # If passthrough sim, use sim's ip address.
     loc_service = LocalizationService(
         host=LOCALIZATION_SERVER_IP, port=LOCALIZATION_SERVER_PORT
-    )  # if passthrough sim, use sim's ip address.
+    )
     rep_service = ReportingService(host=SCORE_SERVER_IP, port=SCORE_SERVER_PORT)
-    # === Initialize pose filter to smooth out noisy pose data ===
-    pose_filter = SimpleMovingAverage(n=3)  # Smoothens out noisy localization data.
-
-    # === Initialize AI services. ===
-    if cfg["use_real_models"] == True:
-        reid_service = REID_SERVICE(
-            yolo_model_path=CV_MODEL_DIR, reid_model_path=REID_MODEL_DIR
-        )
-        speakerid_service = SPEAKER_SERVICE(model_dir=SPEAKER_ID_MODEL_DIR)
-        digit_detection_service = DIGIT_SERVICE(model_dir=NLP_MODEL_DIR)
-    else:  # initialize mock AI classes.
-        reid_service = MockObjectReIDService(
-            yolo_model_path=CV_MODEL_DIR, reid_model_path=REID_MODEL_DIR
-        )
-        speakerid_service = MockSpeakerIDService(model_dir=SPEAKER_ID_MODEL_DIR)
-        digit_detection_service = MockDigitDetectionService(model_dir=NLP_MODEL_DIR)
 
     # === Initialize robot ===
     robot = Robot()
@@ -121,27 +47,31 @@ def main():
 
     # === Initialize planner ===
     map_: SignedDistanceGrid = loc_service.get_map()
-    # TODO: Set map_.scale below to calibrate
-    # TODO: Create manual/automatic calibration routine
-    # map_.scale *= 2
-    map_ = map_.dilated(
-        ROBOT_RADIUS_M
-    )  # dilate obstacles virtually so that planner avoids
-    # bringing robot too close to real obstacles.
+    # Dilate obstacles virtually so that planner avoids bringing robot too close
+    # to real obstacles.
+    map_ = map_.dilated(ROBOT_RADIUS_M)
     planner = Planner(map_, sdf_weight=0.5)
 
-    # === Initialize your own variables ===
-    curr_loi: RealLocation = None  # Current location of interest.
-    path: List[
-        RealLocation
-    ] = []  # list of way points to get from starting location to location of interest.
-    new_loi = None  # new RealLocation received from Reporting Server.
-    target_rotation = None  # a bearing between [-180, 180]
+    # === Loop State/Flags ===
+    # Current location of interest.
+    curr_loi: RealLocation = None
+    # List of way points to get from starting location to location of interest.
+    path: List[RealLocation] = []
+    # New RealLocation received from Reporting Server.
+    new_loi = None
     prev_loi = new_loi
+    # A bearing between [-180, 180].
+    target_rotation = None
+    # Whether to try and start AI tasks.
+    try_start_tasks = False
+
+    # === Initialize pose filter to smooth out noisy pose data ===
+    pose_filter = SimpleMovingAverage(n=3)
 
     navigator = Navigator(map_, robot, loc_service, planner, pose_filter, cfg)
 
-    try_start_tasks = False
+    # TODO: Run initialization of AI services concurrently.
+    ai_loop = prepare_ai_loop(cfg, rep_service, navigator)
 
     # Tell reporting server we are ready.
     res = rep_service.start_run()
@@ -223,105 +153,10 @@ def main():
 
             # TEMP
             # try_start_tasks = True
+            # AI Loop.
             if try_start_tasks:
-                main_log.info("===== Starting AI tasks =====")
-
-                robot.chassis.drive_speed(x=0.0, y=0.0, z=0.0)
-                time.sleep(3)
-
-                # === OBJECT REIDENTIFICATION - Friend or Foe (Visual) ===
-                # if reach checkpoint for first time, curr task is object re-id
-
-                # TAKE PHOTO
-                print("\nRobot taking photo...")
-                with enable_camera(robot, PHOTO_DIR) as take_photo:
-                    img = take_photo(robot, PHOTO_DIR)
-
-                sus_bbox, hostage_bbox = reid(
-                    reid_service, hostage_img, suspect_img, img
-                )
-                # pass images to reid model to identify if scene contains hostage or suspect.
-
-                # Report to scoring server on who was found.
-                # NOTE: The scoring server implementation assumes that the suspect and hostage
-                # will not be in same scene together. If they inadvertently are, then report the
-                # closer bounding box.
-
-                if VISUALIZE:
-                    img_mat = imutils.resize(img, width=1280)
-                    sus_col = (255, 0, 0)
-                    hostage_col = (0, 255, 0)
-                    non_col = (0, 0, 255)
-
-                # TODO: also report the bbox.
-                if sus_bbox:
-                    save_path = rep_service.report_situation(
-                        img, pose, "suspect", Path(ZIP_SAVE_DIR)
-                    )
-                    if VISUALIZE:
-                        cv2.rectangle(img_mat, sus_bbox, sus_col, 5)
-                if hostage_bbox:
-                    save_path = rep_service.report_situation(
-                        img, pose, "hostage", Path(ZIP_SAVE_DIR)
-                    )
-                    if VISUALIZE:
-                        cv2.rectangle(img_mat, hostage_bbox, hostage_col, 5)
-                if not sus_bbox and not hostage_bbox:
-                    save_path = rep_service.report_situation(
-                        img, pose, "none", Path(ZIP_SAVE_DIR)
-                    )
-
-                if VISUALIZE:
-                    cv2.imshow("Object View", img_mat)
-                    cv2.waitKey(1)
-                    # continue
-
-                main_log.info(f"saved received files into {save_path}")
-
-                ## === SPEAKER IDENTIFICATION - Friend or Foe (Audio) ===
-
-                ## identify all speakers in a given directory
-                speakerid_result = identify_speakers(save_path, speakerid_service)
-
-                speakerid_submission = None
-                for audio_fname, speakerid in speakerid_result.items():
-                    if audio_fname.endswith(".wav"):
-                        curr_audio_fname = audio_fname[:-4]
-                        team_and_member = speakerid.split("_")
-                        team = team_and_member[0]
-                        if (
-                            not team.lower() == MY_TEAM_NAME.lower()
-                        ):  # find opponent's clip.
-                            speakerid_submission = curr_audio_fname + "_" + speakerid
-                            break
-
-                if speakerid_submission is not None:
-                    main_log.info(
-                        f"submitting {speakerid_submission} to report_audio API."
-                    )
-                    save_path = rep_service.report_audio(
-                        pose, speakerid_submission, Path(ZIP_SAVE_DIR)
-                    )
-                    main_log.info(f"saved received files into {save_path}")
-                else:
-                    raise Exception("no valid speakerid was found.")
-
-                # === DECODING DIGITS ===
-                digits_result = detect_digits(digit_detection_service, save_path)
-                password = tuple([val for _, val in digits_result.items()])
-
-                # submit answer to scoring server and get scoring server's response.
-                main_log.info(f"Submitting password {password} to report_digit API.")
-                target_pose = rep_service.report_digit(pose, password)
-                main_log.info(f"new target pose received from server: {target_pose}")
-
-                new_loi = RealLocation(x=target_pose[0], y=target_pose[1])
-                target_rotation = target_pose[2]
-
+                new_loi, target_rotation = ai_loop()
                 try_start_tasks = False
-                main_log.info(
-                    "===== Ending AI tasks. Continuing Navigation to new target pose ======"
-                )
 
         # Navigation loop.
         navigator.navigation_loop(last_valid_pose, new_loi, target_rotation)
@@ -349,30 +184,10 @@ if __name__ == "__main__":
     with open(cfg_path, "r") as f:
         cfg = yaml.safe_load(f)
 
-        if cfg["use_real_models"] == True:
-            from .services import (
-                BasicObjectReIDService,
-                NeMoSpeakerIDService,
-                WhisperDigitDetectionService,
-            )
-
-            REID_SERVICE = BasicObjectReIDService
-            SPEAKER_SERVICE = NeMoSpeakerIDService
-            DIGIT_SERVICE = WhisperDigitDetectionService
+        if cfg["use_real_localization"]:
+            from robomaster.robot import Robot
         else:
-            from .services.mock import (
-                MockDigitDetectionService,
-                MockObjectReIDService,
-                MockSpeakerIDService,
-            )
-
-        if cfg["use_real_localization"] == True:
-            import robomaster
-            from robomaster.robot import Robot  # Use this for real robot.
-        else:
-            from tilsdk.mock_robomaster.robot import (
-                Robot,  # Use this for simulated robot.
-            )
+            from tilsdk.mock_robomaster.robot import Robot
 
         VISUALIZE = cfg["VISUALIZE_FLAG"]
 
@@ -385,19 +200,5 @@ if __name__ == "__main__":
         REACHED_THRESHOLD_M = cfg["REACHED_THRESHOLD_M"]
         ANGLE_THRESHOLD_DEG = cfg["ANGLE_THRESHOLD_DEG"]
         ROBOT_RADIUS_M = cfg["ROBOT_RADIUS_M"]
-        NLP_MODEL_DIR = cfg["NLP_MODEL_DIR"]
-        CV_MODEL_DIR = cfg["CV_MODEL_DIR"]
-        REID_MODEL_DIR = cfg["REID_MODEL_DIR"]
-        SPEAKER_ID_MODEL_DIR = cfg["SPEAKER_ID_MODEL_DIR"]
-        PHOTO_DIR = cfg["PHOTO_DIR"]
-
-        ZIP_SAVE_DIR = cfg["ZIP_SAVE_DIR"]
-
-        # Initialize my team name.
-        MY_TEAM_NAME = cfg["MY_TEAM_NAME"]
-
-        # Initialize target images of suspect and target hostage.
-        suspect_img = cv2.imread(cfg["SUSPECT_IMG"])
-        hostage_img = cv2.imread(cfg["HOSTAGE_IMG"])
 
     main()
---
+++ til_23_finals/ai.py
@@ -0,0 +1,196 @@
+"""Handle AI phase of robot."""
+
+import logging
+from pathlib import Path
+from time import sleep
+
+import cv2
+import imutils
+import numpy as np
+from tilsdk.localization import RealLocation
+from tilsdk.mock_robomaster.robot import Robot
+from tilsdk.reporting.service import ReportingService
+
+from til_23_finals.navigation import Navigator
+from til_23_finals.services.abstract import (
+    AbstractDigitDetectionService,
+    AbstractObjectReIDService,
+    AbstractSpeakerIDService,
+)
+from til_23_finals.utils import enable_camera, load_audio_from_dir
+
+reid_log = logging.getLogger("ReID")
+sid_log = logging.getLogger("SpeakID")
+main_log = logging.getLogger("Main")
+
+
+def reid(
+    service: AbstractObjectReIDService,
+    hostage_img: np.ndarray,
+    suspect_img: np.ndarray,
+    img: np.ndarray,
+):
+    """Use reid service to identify whether hostage or suspect is in the image."""
+    sus_bbox = service.targets_from_image(img, suspect_img)
+    hostage_bbox = service.targets_from_image(img, hostage_img)
+    reid_log.info(f"Suspect: {sus_bbox}, Hostage: {hostage_bbox}")
+    return sus_bbox, hostage_bbox
+
+
+def identify_speakers(audio_dir: str, speakerid_service: AbstractSpeakerIDService):
+    """Identify speakers from audio files in audio_dir."""
+    audio_dict = load_audio_from_dir(audio_dir)
+
+    speakerid_result = {}
+    for fname, v in audio_dict.items():
+        audio_waveform, rate = v
+        speakerid_result[fname] = speakerid_service.identify_speaker(
+            audio_waveform, rate
+        )
+
+    for fname, speaker_id in speakerid_result.items():
+        sid_log.info(f"{fname} speaker is {speaker_id}.")
+    return speakerid_result
+
+
+def detect_digits(
+    digit_detection_service: AbstractDigitDetectionService, audio_dir: str
+):
+    """Detect digits from audio files in audio_dir."""
+    digits_result = {}
+    audio_dict = load_audio_from_dir(audio_dir)
+    sorted_fnames = sorted(audio_dict.keys())
+
+    for fname in sorted_fnames:
+        audio_waveform, rate = audio_dict[fname]
+        digits = digit_detection_service.transcribe_audio_to_digits(audio_waveform)
+        digits_result[fname] = (
+            digits[0] if digits else None
+        )  # as a heuristic, take the first digit detected.
+    return digits_result
+
+
+def prepare_ai_loop(cfg, rep: ReportingService, nav: Navigator):
+    """Return function to run AI phase of main loop."""
+    NLP_MODEL_DIR = cfg["NLP_MODEL_DIR"]
+    CV_MODEL_DIR = cfg["CV_MODEL_DIR"]
+    REID_MODEL_DIR = cfg["REID_MODEL_DIR"]
+    SPEAKER_ID_MODEL_DIR = cfg["SPEAKER_ID_MODEL_DIR"]
+
+    PHOTO_DIR = Path(cfg["PHOTO_DIR"])
+    ZIP_SAVE_DIR = Path(cfg["ZIP_SAVE_DIR"])
+    MY_TEAM_NAME = cfg["MY_TEAM_NAME"]
+
+    suspect_img = cv2.imread(cfg["SUSPECT_IMG"])
+    hostage_img = cv2.imread(cfg["HOSTAGE_IMG"])
+
+    VISUALIZE = cfg["VISUALIZE_FLAG"]
+
+    if cfg["use_real_models"]:
+        from til_23_finals.services import (
+            BasicObjectReIDService,
+            NeMoSpeakerIDService,
+            WhisperDigitDetectionService,
+        )
+
+        REID_SERVICE: type = BasicObjectReIDService
+        SPEAKER_SERVICE: type = NeMoSpeakerIDService
+        DIGIT_SERVICE: type = WhisperDigitDetectionService
+
+    else:
+        from til_23_finals.services.mock import (
+            MockDigitDetectionService,
+            MockObjectReIDService,
+            MockSpeakerIDService,
+        )
+
+        REID_SERVICE = MockObjectReIDService
+        SPEAKER_SERVICE = MockSpeakerIDService
+        DIGIT_SERVICE = MockDigitDetectionService
+
+    main_log.info("===== Loading AI services =====")
+    main_log.warning("This will take a while unless we implement concurrent loading!")
+    reid_service = REID_SERVICE(CV_MODEL_DIR, REID_MODEL_DIR)
+    speaker_service = SPEAKER_SERVICE(SPEAKER_ID_MODEL_DIR)
+    digit_service = DIGIT_SERVICE(NLP_MODEL_DIR)
+
+    def loop(robot: Robot):
+        """Run AI phase of main loop."""
+        main_log.info("===== Starting AI tasks =====")
+
+        robot.chassis.drive_speed()
+        # TODO: Test if necessary.
+        # sleep(1)
+        # TODO: Will get_filtered_pose in the future use the calibration routine?
+        pose = nav.get_filtered_pose()
+
+        main_log.info("===== Object ReID =====")
+
+        with enable_camera(robot, PHOTO_DIR) as take_photo:
+            img = take_photo()
+
+        # TODO: Return all bboxes for camera adjustment.
+        # TODO: Return confidence & similarity scores since only one of suspect or hostage can be present.
+        sus_bbox, hostage_bbox = reid(reid_service, hostage_img, suspect_img, img)
+
+        if VISUALIZE:
+            img_mat = imutils.resize(img, width=1280)
+            sus_col = (255, 0, 0)
+            hostage_col = (0, 255, 0)
+            non_col = (0, 0, 255)
+
+        if sus_bbox:
+            save_path = rep.report_situation(img, pose, "suspect", ZIP_SAVE_DIR)
+            if VISUALIZE:
+                cv2.rectangle(img_mat, sus_bbox, sus_col, 5)
+        if hostage_bbox:
+            save_path = rep.report_situation(img, pose, "hostage", ZIP_SAVE_DIR)
+            if VISUALIZE:
+                cv2.rectangle(img_mat, hostage_bbox, hostage_col, 5)
+        if not sus_bbox and not hostage_bbox:
+            save_path = rep.report_situation(img, pose, "none", ZIP_SAVE_DIR)
+
+        if VISUALIZE:
+            cv2.imshow("Object View", img_mat)
+            cv2.waitKey(1)
+
+        main_log.info(f"Saved next task files: {save_path}")
+        main_log.info("===== Speaker ID =====")
+
+        speaker_results = identify_speakers(save_path, speaker_service)
+        speakerid_submission = None
+        for audio_fname, speakerid in speaker_results.items():
+            if audio_fname.endswith(".wav"):
+                curr_audio_fname = audio_fname[:-4]
+                team_and_member = speakerid.split("_")
+                team = team_and_member[0]
+                if not team.lower() == MY_TEAM_NAME.lower():  # find opponent's clip.
+                    speakerid_submission = curr_audio_fname + "_" + speakerid
+                    break
+
+        if speakerid_submission is not None:
+            main_log.info(f"Submitting {speakerid_submission} to report_audio API.")
+            save_path = rep.report_audio(pose, speakerid_submission, ZIP_SAVE_DIR)
+        else:
+            raise Exception("no valid speakerid was found.")
+
+        main_log.info(f"Saved next task files: {save_path}")
+        main_log.info("===== Digit Detection =====")
+
+        digits_result = detect_digits(digit_service, save_path)
+        password = tuple([val for _, val in digits_result.items()])
+
+        # submit answer to scoring server and get scoring server's response.
+        main_log.info(f"Submitting password {password} to report_digit API.")
+        target_pose = rep.report_digit(pose, password)
+        main_log.info(f"new target pose received from server: {target_pose}")
+
+        new_loi = RealLocation(x=target_pose[0], y=target_pose[1])
+        target_rotation = target_pose[2]
+
+        main_log.info(
+            "===== Ending AI tasks. Continuing Navigation to new target pose ======"
+        )
+        return new_loi, target_rotation
+
+    return loop
