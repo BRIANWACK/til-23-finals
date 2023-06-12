2b6f8477e287d997b56385e1120e67f6a0c9c09c
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-06-12 11:53:57 +0800

    improve voice extraction a lot

--- til_23_finals/voice.py
+++ til_23_finals/voice.py
@@ -1,5 +1,7 @@
 """Code to extract clean voice from any noisy audio file."""
 
+from typing import Optional
+
 import torch
 import torch.nn as nn
 from demucs.apply import apply_model
@@ -25,29 +27,45 @@ def load_demucs_model(name=DEMUCS_MODEL, repo=DEMUCS_MODEL_REPO):
 class VoiceExtractor(nn.Module):
     """Class to extract voice from audio file."""
 
-    # TODO: Expose below for configuration.
-    spectral_gate_std_thres = 1.5
-    spectral_noise_remove = 0.98
-    spectral_n_fft = 1024
-
-    def __init__(self, sr: int = DEFAULT_SR, model=None):
+    # TODO: Expose below kwargs for tuning in the config file.
+    def __init__(
+        self,
+        sr: int = DEFAULT_SR,
+        spectral_gate_std_thres: float = 1.0,
+        spectral_noise_remove: float = 1.0,
+        spectral_n_fft: int = 512,
+        spectral_freq_mask_hz: Optional[int] = None,
+        spectral_time_mask_ms: Optional[int] = None,
+        skip_demucs: bool = False,
+        skip_denoise1: bool = False,
+        use_ori: bool = False,
+        model=None,
+    ):
         super(VoiceExtractor, self).__init__()
         # See: https://github.com/timsainb/noisereduce.
         self.denoise1 = TG(
             sr=sr,
             nonstationary=False,
-            n_std_thresh_stationary=self.spectral_gate_std_thres,
-            prop_decrease=self.spectral_noise_remove,
-            n_fft=self.spectral_n_fft,
-            # Below smooth out the noise mask to prevent artifacting.
-            freq_mask_smooth_hz=300,
-            time_mask_smooth_ms=100,
+            n_std_thresh_stationary=spectral_gate_std_thres,
+            prop_decrease=spectral_noise_remove,
+            n_fft=spectral_n_fft,
+            # Below smoothes out the noise mask to prevent artifacting.
+            # NOTE: Given a good noise sample, there is no artifacting even with
+            # the masks disabled.
+            freq_mask_smooth_hz=spectral_freq_mask_hz,
+            time_mask_smooth_ms=spectral_time_mask_ms,
         )
         self.demucs = load_demucs_model() if model is None else model
+        self.skip_demucs = skip_demucs
+        self.skip_denoise1 = skip_denoise1
+        self.use_ori = use_ori
 
-    def _denoise1(self, wav: torch.Tensor, sr: int):
+    def _denoise1(
+        self, wav: torch.Tensor, sr: int, noise: Optional[torch.Tensor] = None
+    ):
         wav = resample(wav, orig_freq=sr, new_freq=self.denoise1.sr)
-        wav = self.denoise1(wav[None])[0]
+        noise = noise[None] if noise is not None else None
+        wav = self.denoise1(wav[None], noise)[0]
         return wav, self.denoise1.sr
 
     def _demucs(self, wav: torch.Tensor, sr: int):
@@ -59,7 +77,7 @@ class VoiceExtractor(nn.Module):
         sources = apply_model(
             self.demucs,
             wav[None],  # BCT
-            shifts=1,
+            shifts=2,
             split=True,
             progress=False,
             overlap=0.25,
@@ -75,6 +93,23 @@ class VoiceExtractor(nn.Module):
         """Extract voice from audio file."""
         assert len(wav.shape) == 1, "Input must be T."
 
-        wav, sr = self._demucs(wav, sr)
-        wav, sr = self._denoise1(wav, sr)
+        ori_wav, ori_sr = wav, sr
+        noise = None
+        if not self.skip_demucs:
+            wav, sr = self._demucs(wav, sr)
+            # Use extracted voice sample to find noise sample.
+            noisy = (
+                ori_wav
+                if self.use_ori
+                else resample(ori_wav, orig_freq=ori_sr, new_freq=sr)
+            )
+            voice = (
+                resample(wav, orig_freq=sr, new_freq=ori_sr) if self.use_ori else wav
+            )
+            noise = noisy - voice
+        if not self.skip_denoise1:
+            if self.use_ori:
+                wav, sr = self._denoise1(ori_wav, ori_sr, noise)
+            else:
+                wav, sr = self._denoise1(wav, sr, noise)
         return wav, sr
