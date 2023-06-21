bb05863cc5d9d88cad5d5c6b531f277d6ae2c94b
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-06-21 19:21:24 +0800

    env.sh can find & source venv too

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Find and add cuda & cudnn from virtual environment wheels and add to LD_LIBRARY_PATH
-# TODO: Some of the below has to be inserted manually.
-# VENV_DIR="venv"
-VENV_DIR="/home/advanced-brianwack/Desktop/"
-# VENV_DIR="/home/jh2xl/.conda/envs/python38"
-CUDNN_LIB=$(find $VENV_DIR -type d -path "*/site-packages/nvidia/cudnn/lib")
+# Find and add cuda & cudnn to LD_LIBRARY_PATH.
+# SEARCH_DIR="venv"
+# SEARCH_DIR="/home/jh2xl/.conda/envs/python38"
+SEARCH_DIR="/home/advanced-brianwack/Desktop/"
+ACTIVATE_PATH=$(find $SEARCH_DIR -type f -name "bin/activate")
+source $ACTIVATE_PATH
+CUDNN_LIB=$(find $SEARCH_DIR -type d -path "*/site-packages/nvidia/cudnn/lib")
 export LD_LIBRARY_PATH=$CUDNN_LIB:/usr/lib64:/usr/lib:/lib:/lib64:$LD_LIBRARY_PATH
