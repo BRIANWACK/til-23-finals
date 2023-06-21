bbaf9d053c0801c61d2df34ef1ac705d37ef90dc
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-06-21 12:05:56 +0800

    fix & use fasterwhisper

--- env.sh
+++ env.sh
@@ -2,8 +2,7 @@
 # Find and add cuda & cudnn from virtual environment wheels and add to LD_LIBRARY_PATH
 # TODO: Some of the below has to be inserted manually.
 # VENV_DIR="venv"
-VENV_DIR="/home/jh/.conda/envs/temp/lib/python3.9"
+VENV_DIR="/home/brianwack-advanced/Desktop/TIL/venv"
+# VENV_DIR="/home/jh2xl/.conda/envs/python38"
 CUDNN_LIB=$(find $VENV_DIR -type d -path "*/site-packages/nvidia/cudnn/lib")
-# CUDA_LIB=/usr/lib/nvidia/lib
-CUDA_LIB="/lib/wsl/lib"
-export LD_LIBRARY_PATH=$CUDNN_LIB:$CUDA_LIB:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUDNN_LIB:/usr/lib64:/usr/lib:/lib:/lib64:$LD_LIBRARY_PATH
