c5ee43fafa6bf18476d9599a2095350557f16811
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-06-21 20:12:16 +0800

    fix env activate script

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 # SEARCH_DIR="venv"
 # SEARCH_DIR="/home/jh2xl/.conda/envs/python38"
 SEARCH_DIR="/home/advanced-brianwack/Desktop/"
-ACTIVATE_PATH=$(find $SEARCH_DIR -type f -name "bin/activate")
+ACTIVATE_PATH=$(find $SEARCH_DIR -type f -path "*/bin/activate")
 source $ACTIVATE_PATH
 CUDNN_LIB=$(find $SEARCH_DIR -type d -path "*/site-packages/nvidia/cudnn/lib")
 export LD_LIBRARY_PATH=$CUDNN_LIB:/usr/lib64:/usr/lib:/lib:/lib64:$LD_LIBRARY_PATH
